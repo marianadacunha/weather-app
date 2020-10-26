d5ca567e6b2f5b609b4ed2dde93d85b1a16e098d
Mariana Marques <marianadacunhamarques@gmail.com>
2020-10-26 15:36:57 -0300

    add SASS

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function fetchWeatherData() {
+    let temperature = document.getElementById("temperature");
+    let description = document.getElementById("description");
+    let location = document.getElementById("location");
+    let api = "https://api.openweathermap.org/data/2.5/weather";
+    let apiKey = "0c829e5b400bfad68fe2d1d7f0b2e066";
+}
