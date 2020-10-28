243a77a94a51f5f279ff5b32a0c498fef45256e0
Mariana Marques <marianadacunhamarques@gmail.com>
2020-10-28 12:40:02 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -2,6 +2,30 @@ const form = document.querySelector(".city-search .container form");
 const input = document.querySelector(".city-search .container input");
 const message = document.querySelector(".city-search .container .message");
 const list = document.querySelector(".city-display .container .cities");
+const IMAGES = [
+  { "01d": 'img/01d.png' },
+  { "01n": 'img/01n.png' },
+  { "02d": 'img/02d.png' },
+  { "02n": 'img/02n.png' },
+  { "03d": 'img/03d.png' },
+  { "03n": 'img/03n.png' },
+  { "04d": 'img/04d.png' },
+  { "04n": 'img/04n.png' },
+  { "09d": 'img/09d.png' },
+  { "09n": 'img/09n.png' },
+  { "10d": 'img/10d.png' },
+  { "10n": 'img/10n.png' },
+  { "11d": 'img/11d.png' },
+  { "11n": 'img/11n.png' },
+  { "13d": 'img/13d.png' },
+  { "13n": 'img/13n.png' },
+  { "50d": 'img/50d.png' },
+  { "50n": 'img/50n.png' }
+]
+
+function getImage(iconName) {
+  return `img/${iconName}.png`
+}
 
 form.addEventListener("submit", e => {
     e.preventDefault(); // Prevents reloading the page
@@ -20,7 +44,7 @@ form.addEventListener("submit", e => {
                 weather
             } = data;
 
-            const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+            const icon = getImages(weather[0]["icon"]);
             const li = document.createElement("li");
 
             li.classList.add("city");
